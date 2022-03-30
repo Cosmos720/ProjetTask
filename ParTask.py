fd56770e05b78c47d11beb35ef6cd8cd59eeb7f1
Cosmos720 <corentintitboux@gmail.com>
2022-03-30 23:11:16 +0200

    Projet Systeme d'exploitation fonctionnel

--- ParTask.py
+++ ParTask.py
@@ -1,4 +1,9 @@
-from threading import Thread
+from ast import Global
+from concurrent.futures import thread
+from threading import Thread, Semaphore
+import pygraphviz as pg
+
+
 
 class Task:
     name = ""
@@ -6,29 +11,65 @@ class Task:
     writes = []
     run = None
 
-    def __init__(self, n, r, w, f):
+    def __init__(self, n, f, r = [], w = []):
         self.name=n
         self.reads=r
         self.writes=w
         self.run=f
 
+    def __repr__(self):
+        return self.name
+
 class TaskSystem:
     taskList = []
-    dico = {}
+    ordre = {}
     dependance = {}
+    effectuer = []
+    sem = Semaphore(0)
+    threads = []
 
-    def __init__(self, taskList,dico):
+    def __init__(self, taskList):
         self.taskList = taskList
-        self.dico = dico
+        self.ordre = {task:[] for task in self.taskList}
+        self.calcDico()
         self.calcDepedencies()
+        self.sem = Semaphore(len(self.taskList))
+
+    def calcDico(self):
+        for task in range(len(self.taskList)):
+            for dep in range(task,-1,-1):
+                self.ordre[self.taskList[task]] += [self.taskList[dep]]
+
 
     def calcDepedencies(self):
         for t1 in self.taskList:
             dep=[]
-            for t2 in self.dico[t1.name]:
+            for t2 in self.ordre[t1]:
                 if t1 != t2 and not(self.Bernstein(t1, t2)):
-                    dep += [t2.name]
-            self.dependance[t1.name] = dep
+                    dep += [t2]
+            self.dependance[t1] = dep
+        for key in self.dependance.keys():
+            redond = []
+            for value in self.dependance[key]:
+                if self.dependance[value] != []:
+                    inter = list(set(self.dependance[key]) & set(self.dependance[value]))
+                    for x in inter:
+                        if not(redond.__contains__(x)):
+                            redond.append(x)
+            for x in redond:
+                self.dependance[key].remove(x)
+
+
+
+    def test(self):
+        graph = pg.AGraph(directed=True)
+        for key in self.dependance.keys():
+            if not (self.dependance[key]==[]):
+                for value in self.dependance[key]:
+                    graph.add_edge(value.name, key.name)
+        graph.layout('dot')
+        graph.draw('Graphe.png')
+        graph.close()
                 
     
     def Bernstein(self, t1, t2):
@@ -39,9 +80,52 @@ class TaskSystem:
         return False
     
     def getDepedencies(self, nomTask):
-        print(self.dependance)
+        dep = []
+        for key, value in self.dependance.items():
+            if nomTask==key:
+                for v in value:
+                    dep.append(v)
+                return dep
+        
+    def speedrun(self, task):
+        taskWait = []
+        for t in self.getDepedencies(task):
+            taskWait.append(t.name)
+            if not(self.effectuer.__contains__(t)):
+                self.effectuer.append(t)
+                thread = Thread(name=t.name, target=self.speedrun, args=[t])
+                thread.start()
+                self.threads.append(thread)
+        
+        for t in self.threads:
+            if taskWait.__contains__(t.getName()):
+                t.join()
+        
+        task.run()
+        print(task.name + " à fini de s'executer")
 
     def run(self):
-        for t in self.taskList:
-            t.run()
-            
+        self.test()
+        tasks = []
+        for task in self.dependance.keys():
+            last = True
+            for t in self.dependance.keys():
+                if task != t:
+                    if self.dependance[t].__contains__(task):
+                        last = False
+                        break
+            if last:
+                tasks.append(task)
+        self.effectuer += tasks
+
+        taskWait = []
+        for task in tasks:
+            taskWait.append(task.name)
+            thread = Thread(name=task.name, target=self.speedrun, args=[task])
+            thread.start()
+            self.threads.append(thread)
+
+        for t in self.threads:
+            if taskWait.__contains__(t.getName()):
+                t.join()
+        
--- Test.py
+++ Test.py
@@ -1,32 +1,51 @@
 from ParTask import *
 
+W = None
 X = None
 Y = None
 Z = None
 
+def runT0():
+    global X
+    X=X
 def runT1():
     global X
-    X = 1
-
+    X = 25
 def runT2():
     global Y
-    Y = 2
-
+    Y = 3
+def runT3():
+    global Y
+    Y = Y
+def runT4():
+    global X
+    X = X
 def runTsomme():
-    
     global X, Y, Z
     Z = X + Y
+def runT5():
+    global X, Y, W
+    W = X + Y
 
-t1 = Task("T1", [], ["X"], runT1)
+t0 = Task("T0", runT0, r=["X"])
 
-t2 = Task("T2", [], ["Y"], runT2)
+t1 = Task("T1", runT1, w=["X"])
 
-tSomme = Task("somme", ["X","Y"], ["Z"], runTsomme)
+t2 = Task("T2", runT2, w=["Y"])
 
-s1 = TaskSystem([t1, t2, tSomme], {"T1": [], "T2": [t1], "somme": [t1, t2]})
+t3 = Task("T3", runT3, ["Y"], ["Y"])
 
-print(s1.getDepedencies("somme"))
+t4 = Task("T4", runT4, ["X"], ["X"])
+
+tSomme = Task("somme", runTsomme, ["X", "Y"], ["Z"])
+
+t5 = Task("T5", runT5, ["X", "Y"], ["W"])
+
+s1 = TaskSystem([t1, t2, t3, t4, tSomme, t5])
 
 s1.run()
 
+print(X)
+print(Y)
 print(Z)
+print(W)
